6bdbf649cdb9e3d9ce7e453d1dc8b4f5e15e2bd8
premkumarv-eng <premkumar.v@vvdntech.com>
2026-01-19 12:24:12 +0530

    code for security group deletion

--- basic_tasks.py
+++ basic_tasks.py
@@ -5,19 +5,23 @@ import time
 
 LOGIN_URL = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/authenticate.php'
 SECURITY_GROUPS_URL = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/admin/securityGroupList.php'
+ALL_SECURITY_GROUPS = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/securityGroupList.php'
 SSID_CREATION_URL = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/securityGroupAdd.php'
-SSID_DELETION_URL = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/securityGroupList.php'
+SSID_DELETION_URL = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/securityGroupList.php?context=delete'
 
 class UnifasAutomator:
     def __init__(self):
         self.csrf_token = None
         self.session = requests.Session()
         self.login_data = credentials.login_data
-        # self.request_headers = {'Content-Type': 'application/json'}
+        self.request_headers = {'Content-Type': 'application/json'}
 
     def login(self):
-        import pdb; pdb.set_trace()
         login_session = self.session.post(LOGIN_URL, data=self.login_data)
+        self.session.headers.update({
+                                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+                                        "Accept": "application/json, text/javascript, */*; q=0.01",
+                                        "X-Requested-With": "XMLHttpRequest"})
         return login_session.ok
 
     def create_csrf_token(self):
@@ -38,24 +42,30 @@ class UnifasAutomator:
         ssid_session = self.session.post(SSID_CREATION_URL, params=query_params, data=ssid_data)
         return ssid_session.ok
 
-    def delete_security_group(self, ssid_name):
-        # Todo
-        ssid_data = None
-        '''csrfToken
-        4edb9550294a3e3654650571292d2cd92271b33df32a0c2c
-        sg-list-table_length
-        300
-        sg_delete[]
-        2999
-        '''
-        query_params = {"context": "save"}
+    def delete_security_group(self, ssid_name, group_name):
+        group_id = None
+        security_groups = unifas.get_security_group_list()
+        for security_group in security_groups:
+            if security_group[3] == ssid_name:
+                group_id = re.findall(r'value=.*(\d{4})', "".join(security_group))
+                break
+        if group_id is None:
+            print("SSID not deleted")
+            return None
+        data = {'csrfToken': self.csrf_token, 'sg-list-table_length': '300', 'sg_delete[]': f'{group_id[0]}'}
+        result = self.session.post(SSID_DELETION_URL, data=data)
+        result = result.json()
+        if result['success'] and group_name in result['message']:
+            return True
+        return False
 
     def get_security_group_list(self):
-        # Todo
-        url = 'https://uf17-fdev.unifas.jp/UNIFAS/MS/webapi/v1/admin/securityGroupList.php'
-        query_params = {"_": int(time.time())}
-        get_session = self.session.get(url, params=query_params)
+        query_params = {"_": int(time.time() * 1000)}
+        get_session = self.session.get(ALL_SECURITY_GROUPS, params=query_params)
         print(get_session)
+        if get_session.ok:
+            return get_session.json()['data']
+        return None
 
     def verify_session_and_rebuild(self, build_csrf_token):
         if not self.session:
@@ -67,6 +77,6 @@ class UnifasAutomator:
 if __name__ == '__main__':
     unifas = UnifasAutomator()
     unifas.login()
-    unifas.get_security_group_list()
     unifas.create_csrf_token()
-    unifas.create_new_security_group("HydTestConnection", "HydTestGroup", "HydTestConnection")
+    unifas.create_new_security_group("HydTestConnection2", "HydTestGroup", "HydTestConnection")
+    unifas.delete_security_group("HydTestConnection2", 'HydTestGroup')
